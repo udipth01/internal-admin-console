f4bdbbefef5bb3e462d165a1ea54ab7df527ca82
udipth01 <udipth@gmail.com>
2025-12-30 11:42:35 +0530

    corrected error in login

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routes import login, tables
 
 app = FastAPI()
+
+# 🔥 THIS IS MISSING 🔥
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(login.router)
 app.include_router(tables.router)
